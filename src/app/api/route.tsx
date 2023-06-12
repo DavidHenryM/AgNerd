f21689e926e812470fe652b1a19da446c828eab7
David Moorhouse <david.moorhouse@thecreativeelement.com.au>
2023-06-12 17:21:52 +1000

    added bearer auth

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -2,18 +2,35 @@ import 'reflect-metadata';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
 import { NextRequest } from 'next/server';
-import {resolvers} from '../../prisma/generated'
+import  {resolvers} from '../../prisma/generated'
 import { buildSchemaSync } from 'type-graphql'
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv';
+import { GraphQLError } from 'graphql/error/GraphQLError';
 
+dotenv.config()
 
 const prisma = new PrismaClient();
 
 
 const schema = buildSchemaSync({
-    resolvers,
+    resolvers: [...resolvers],
     validate: false,
   });
+
+
+async function authorize(request: NextRequest): Promise<boolean> {
+  const bearerTokenString: string | undefined = process.env.BEARER_TOKENS;
+  if (bearerTokenString) {
+    const bearerTokens: Array<string> = bearerTokenString.split(',');
+    const bearerToken: string = request.headers.get('authorization') || '';
+    const token = bearerToken.split('Bearer ')[1];
+    if (bearerTokens.includes(token)) {
+      return true
+    }
+  }
+  return false;
+} 
 
 
 const server = new ApolloServer({
@@ -21,8 +38,20 @@ const server = new ApolloServer({
 });
 
 
-// req has the type NextRequest
-const handler = startServerAndCreateNextHandler<NextRequest>(server, { context: async req => ({ req, prisma }) });
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async request => { 
+    if (! await authorize(request)){
+      throw new GraphQLError('Not authorized', {
+        extensions: {
+          code: 'UNAUTHORISED',
+          http: { status: 403 },
+        },
+      });
+    }
+
+    return {request, prisma}
+   }
+ });
 
 export async function GET(request: NextRequest) {
   return handler(request);
