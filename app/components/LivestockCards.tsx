533bd919cda559a102be78580c528c9869bef80e
David Moorhouse <david@locusinnovations.com.au>
2024-12-28 12:43:24 +1100

    refactor functions to seperate files

--- app/components/LivestockCards.tsx
+++ app/components/LivestockCards.tsx
@@ -1,156 +1,44 @@
 "use client"
 
-import { ActiveLivestock } from '../basicDetails'
-import { GiBull, GiCow, GiFemale, GiHelp, GiMale } from 'react-icons/gi'
-import { HiScissors } from 'react-icons/hi'
+import { getActiveLivestock } from '../queries'
 import { 
-    Card, 
-    CardHeader, 
-    CardBody, 
-    Text, 
-    Divider, 
-    Tag, 
-    TagLabel, 
-    Stat,
-    StatArrow,
-    StatHelpText,
-    StatLabel,
-    StatNumber, 
-    TagLeftIcon,
-    HStack,
-    VStack,
-    Stack,
-    WrapItem,
-    Wrap
-} from '../chakraShim'
-import { IconType } from 'react-icons/lib'
+  HStack,
+  Text,
+  Card
+} from "@chakra-ui/react"
+import { DesexedTag, SexTag, StockClassTag } from './Tags'
+import { WeightStats } from './Stats'
+import { LivestockUnit } from '@prisma/client'
 
-export function WeightStats(props: any){
-    if (props.length > 0){
-        let statArrow: "increase" | "decrease" | undefined
-        if (props.length > 1){
-            console.log("more than one weight found")
-            const statChange = Number(props[-2].weight) - Number(props[-1].weight) 
-            if (statChange >= 0){
-                statArrow = "increase"
-            } else {
-                statArrow = "decrease"
-            }
-            return (
-                <Stat>
-                    <StatLabel>Live weight multi</StatLabel>
-                    <StatNumber>{props[0].weight + "kg"}</StatNumber>
-                    <StatHelpText>
-                        <StatArrow type={statArrow} />
-                        {String(statChange) + "kg"}
-                    </StatHelpText>
-                </Stat> 
-            )
-        } else {
-            return (
-                <Stat>
-                    <StatLabel>Live weight only one</StatLabel>
-                    <StatNumber>{props[0].weight + "kg"}</StatNumber>
-                </Stat>
-                )
-        }
-    } 
-}
 
-function sexTag(sex: string) {
-    let SexIcon: IconType
-    let sexColour: string
-    if(sex == 'FEMALE'){
-        SexIcon = GiFemale
-        sexColour = 'pink'
-    } else if (sex == 'MALE') {
-        SexIcon = GiMale
-        sexColour = 'blue'
-    } else {
-        SexIcon = GiHelp
-        sexColour = 'white'
-    }
-    return (
-        <Tag colorScheme={sexColour}>
-            <HStack spacing='6px'>
-                <SexIcon />
-                <TagLabel>{sex}</TagLabel>
-            </HStack>
-        </Tag>
-    )
-}
 
-function desexedTag(desexed: boolean, sex: string) {
-    let DesexedIcon: IconType
-    let desexedText: String
-    if(desexed){
-        DesexedIcon = HiScissors
-        desexedText = "DESEXED"
-    } else {
-        if (sex == 'MALE'){
-            DesexedIcon = GiBull
-            desexedText = "INTACT"
-        } else {
-            return
-        }
-    }
-    return (
-        <Tag colorScheme='green'>
-            <HStack>
-                <DesexedIcon />
-                <TagLabel>{desexedText}</TagLabel>
-            </HStack>
-        </Tag>
-    )
-}
 
-function stockClassTag(stockClass: String) {
-    if(stockClass == "CATTLE"){
-        return (   
-            <Tag color='burlywood'>
-                <HStack spacing='6px'>
-                    <GiCow />
-                    <TagLabel>{stockClass}</TagLabel>
-                </HStack>
-            </Tag> 
-        )
-    } else {
+
+
+export async function LivestockCards() {
+  const livestocks: LivestockUnit[] = await getActiveLivestock()
+  return (
+    <HStack wrap={"wrap"}>
+      {livestocks.map(function(stock: LivestockUnit, index){
         return (
-            <Tag>
-                <TagLabel>{stockClass}</TagLabel>
-            </Tag>
+          <div key={index}>
+            <Card.Root key={"stockCard" + index}>
+              <Card.Header>
+                <Text>{stock.name}</Text>
+                <Text>{stock.nlisId}</Text>
+              </Card.Header>
+              <Card.Body>
+                <HStack>
+                  {StockClassTag({stockClass: stock.class})}
+                  {SexTag({sex: stock.sex})}
+                  {DesexedTag({desexed: stock.desexed, sex: stock.sex})}
+                </HStack>
+                  {WeightStats({weights: stock.weights})}
+              </Card.Body>
+            </Card.Root>
+          </div>
         )
-    }
-}
-
-// export async function LivestockCards() {
-//     const livestocks = ActiveLivestock()
-
-//     return (
-//         <Wrap>
-//         {livestocks.map(function(stock, index){
-//             return (
-//                 <WrapItem key={index}>
-//                     <Card key={"stockCard" + index}>
-//                         <CardHeader>
-//                             <Text>{stock.name}</Text>
-//                             <Text>{stock.nlisId}</Text>
-//                         </CardHeader>
-//                         <CardBody>
-                            
-//                             <HStack>
-//                                 {stockClassTag(stock.class)}
-//                                 {sexTag(stock.sex)}
-//                                 {desexedTag(stock.desexed, stock.sex)}
-//                             </HStack>
-//                             {weightStats(stock.weights)}
-
-//                         </CardBody>
-//                     </Card>
-//                     {/* <Divider orientation='horizontal'/> */}
-//                 </WrapItem>
-//             )
-//         })}
-//         </Wrap>    
-//     )
-// } 
+      })}
+    </HStack>    
+  )
+} 
