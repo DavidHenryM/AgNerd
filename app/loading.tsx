a716b5356f5dc55bd55c65941fc35531e593cf24
David Moorhouse <david@locusinnovations.com.au>
2024-12-28 15:38:49 +1100

    bar to loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,12 +3,21 @@ import {
   ProgressCircleRing,
   ProgressCircleRoot,
 } from "@/components/ui/progress-circle"
+import { ProgressBar, ProgressRoot } from "@/components/ui/progress"
 
 export function Loading() {
   return (
     <ProgressCircleRoot value={null} size="lg" colorPalette={"teal"}>
       <ProgressCircleRing cap="round" />
     </ProgressCircleRoot>
+  )
+}
+
+export function LoadingBar(){
+  return (
+  <ProgressRoot maxW="80px" value={null}>
+    <ProgressBar />
+  </ProgressRoot>
   )
 }
 
