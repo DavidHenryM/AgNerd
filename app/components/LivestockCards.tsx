5472e6905686e7e1455dfc37199c2d766a01c84e
David Moorhouse <david@locusinnovations.com.au>
2024-12-28 15:39:36 +1100

    use hooks

--- app/components/LivestockCards.tsx
+++ app/components/LivestockCards.tsx
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 
 import { getActiveLivestock } from '../queries'
 import { 
@@ -9,36 +9,52 @@ import {
 import { DesexedTag, SexTag, StockClassTag } from './Tags'
 import { WeightStats } from './Stats'
 import { LivestockUnit } from '@prisma/client'
+import { useEffect, useState } from 'react'
+import Loading from '../loading'
 
 
 
 
 
 
-export async function LivestockCards() {
-  const livestocks: LivestockUnit[] = await getActiveLivestock()
-  return (
-    <HStack wrap={"wrap"}>
-      {livestocks.map(function(stock: any, index){
-        return (
-          <div key={index}>
-            <Card.Root key={"stockCard" + index}>
-              <Card.Header>
-                <Text>{stock.name}</Text>
-                <Text>{stock.nlisId}</Text>
-              </Card.Header>
-              <Card.Body>
-                <HStack>
-                  {StockClassTag({stockClass: stock.class})}
-                  {SexTag({sex: stock.sex})}
-                  {DesexedTag({desexed: stock.desexed, sex: stock.sex})}
-                </HStack>
-                  {WeightStats({weights: stock.weights})}
-              </Card.Body>
-            </Card.Root>
-          </div>
-        )
-      })}
-    </HStack>    
-  )
-} 
+export function LivestockCards() {
+  let [livestocks, setLivestocks] = useState<any[]>([])
+  let [loading, setLoading] = useState<boolean>(true)
+  useEffect(() => {
+    getActiveLivestock()
+      .then((livestocks: LivestockUnit[]) => {
+        setLivestocks(livestocks)
+        console.log(livestocks)
+        setLoading(false)
+      })
+      // })
+  }, [])
+  if (loading){
+    return(<Loading></Loading>)
+  } else {
+    return (
+      <HStack wrap={"wrap"}>
+        {livestocks.map(function(stock: any, index){
+          return (
+            <div key={index}>
+              <Card.Root key={"stockCard" + index}>
+                <Card.Header>
+                  <Text>{stock.name}</Text>
+                  <Text>{stock.nlisId}</Text>
+                </Card.Header>
+                <Card.Body>
+                  <HStack>
+                    {StockClassTag({stockClass: stock.class})}
+                    {SexTag({sex: stock.sex})}
+                    {DesexedTag({desexed: stock.desexed, sex: stock.sex})}
+                  </HStack>
+                    {WeightStats({weights: stock.weights})}
+                </Card.Body>
+              </Card.Root>
+            </div>
+          )
+        })}
+      </HStack>    
+    )
+  } 
+}
