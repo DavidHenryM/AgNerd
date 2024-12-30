3971ab4e9768471c5159de0d9e2a1862f8419b79
David Moorhouse <david@locusinnovations.com.au>
2024-12-30 16:34:58 +1100

    new loading card

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,20 +4,43 @@ import {
   ProgressCircleRoot,
 } from "@/components/ui/progress-circle"
 import { ProgressBar, ProgressRoot } from "@/components/ui/progress"
+import SidebarWithHeader from "./components/SideBar"
+import { AbsoluteCenter, Card, Center, Heading, VStack } from "@chakra-ui/react"
+import { NerdCowboy } from "./components/Images"
 
 export function Loading() {
   return (
-    <ProgressCircleRoot value={null} size="lg" colorPalette={"teal"}>
-      <ProgressCircleRing cap="round" />
-    </ProgressCircleRoot>
+    <LoadingCard></LoadingCard>
   )
 }
 
 export function LoadingBar(){
   return (
-  <ProgressRoot maxW="80px" value={null}>
+  <ProgressRoot width={"80px"} colorPalette={"teal"} value={null}>
     <ProgressBar />
   </ProgressRoot>
+  )
+}
+
+export function LoadingCard() {
+  return (
+    <AbsoluteCenter>
+      <Card.Root variant={"elevated"} minW={"200px"} maxW={"300px"} >
+        <Card.Body>
+          <VStack gap={6}>
+            <Heading>Loading...</Heading>
+            <NerdCowboy size={60}/>
+            <LoadingBar/>
+          </VStack>
+        </Card.Body>
+        <Card.Footer/>
+      </Card.Root>
+</AbsoluteCenter>  )
+}
+
+export function LoadingPage(){
+  return (
+    <SidebarWithHeader Content={LoadingCard}></SidebarWithHeader>
   )
 }
 
