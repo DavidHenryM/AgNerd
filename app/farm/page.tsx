ca6a70972452f8cfc6f0a8a7ca16e29fd3b36968
David Moorhouse <david@locusinnovations.com.au>
2025-01-06 19:53:02 +1100

    moved bits

--- app/farm/page.tsx
+++ app/farm/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FarmCard } from "../components/FarmCard";
+import { FarmCard } from "../components/cards/FarmCard";
 import SidebarWithHeader from "../components/SideBar";
 
 export default function Farm(){
