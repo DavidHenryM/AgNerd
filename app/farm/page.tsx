dc4901fea4585f45a95d5d7075e952cf2c66cbe0
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:27:02 +1100

    ermove uneccessary sidebar

--- app/farm/page.tsx
+++ app/farm/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { FarmCard } from "../components/cards/FarmCard";
-import SidebarWithHeader from "../components/SideBar";
 
 export default function Farm(){
   return (
-    <SidebarWithHeader Content={FarmCard}></SidebarWithHeader>
+    <>
+      <FarmCard />
+    </>   
   )
 }
