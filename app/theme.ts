364e21fd17f833b3ae10b43f81612c6b84b41476
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:23:44 +1100

    updated theme

--- app/theme.ts
+++ app/theme.ts
@@ -1,12 +1,95 @@
-import { createSystem, defaultConfig } from "@chakra-ui/react"
+'use client';
+import { createTheme } from '@mui/material/styles';
 
-export const system = createSystem(defaultConfig, {
-  theme: {
-    tokens: {
-      fonts: {
-        heading: { value: `'Figtree', sans-serif` },
-        body: { value: `'Figtree', sans-serif` },
-      },
-    },
+
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    xxl: true;
+  }
+}
+
+
+const components = {
+  MuiUseMediaQuery: {
+    defaultProps: {
+      noSsr: true,
+    }
+  }
+}
+
+const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+    xxl: 2200
   },
-})
+}
+
+const typography = {
+  fontFamily: "var(--font-arvo)"
+}
+
+export const darkTheme = createTheme(
+  { 
+    components: {
+      ...components,
+      MuiButton: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            color: theme.palette.secondary.main
+          })
+        }
+      }
+    }, 
+    breakpoints: breakpoints,
+    typography: typography,
+    palette: { 
+      mode: 'dark',
+      primary: {
+        main: '#5A6B4F',
+        contrastText: '#ffffff',
+      },
+      secondary: {
+        main: '#89986D',
+        contrastText: '#ffffff',
+      },
+    } 
+  }
+)
+
+export const lightTheme = createTheme(
+  { 
+    components: {
+      ...components,
+      MuiButton: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            color: theme.palette.secondary.main
+          })
+        }
+      }
+    },
+    breakpoints: breakpoints,
+    typography: typography,
+    palette: { 
+      mode: 'light', 
+      primary: {
+        main: '#9CAB84',
+        contrastText: 'rgba(0,0,0,0.89)',
+      },
+      secondary: {
+        main:  '#F6F0D7',
+        contrastText: 'rgba(0,0,0,0.89)',
+      },
+    } 
+  }
+)
+
