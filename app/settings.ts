f8afcbf2bd2522d0dad099cde42d63f2014b84f7
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:23:57 +1100

    ui size settings

---
+++ app/settings.ts
@@ -0,0 +1,4 @@
+export const drawerWidth = {xs: "0px", xsDrawerOpen: "240px", sm: "0px", smDrawerOpen: "240px", md: "50px", lg: "240px"}
+export type DrawerWidth = typeof drawerWidth
+
+export const footerHeight = "100px"
