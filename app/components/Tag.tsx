6bbae6fedfd8de7b58ac562a1faf6a6754f5c711
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:26:17 +1100

    reformatted svg

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -2,7 +2,7 @@ export function Tag() {
   return (
     <div>
       <svg xmlns="http://www.w3.org/2000/svg"  width="400" height="110">
-        <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+        <rect width="300" height="100" style={{fill:"rgb(0,0,255)", strokeWidth:3, stroke:"rgb(0,0,0)"}} />
       </svg>
     </div>
   )
