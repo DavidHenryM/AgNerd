aa7514f589ff7a18797c6c1f8991efcecdee59ae
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:17:02 +1100

    removed unused and deprecated files

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ActiveLivestock } from "./basicDetails";
-import SidebarWithHeader from "./components/SideBar";
-
-export default function Home() {
-  return (
-    <SidebarWithHeader Content={ActiveLivestock}></SidebarWithHeader>
-  );
-}
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { 
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
-};
-
-export default nextConfig;
