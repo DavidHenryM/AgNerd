dcdb0f263e9086dc1f19b9c2f38f4969d338bf05
David Moorhouse <david@locusinnovations.com.au>
2026-02-01 23:28:13 +1100

    updated for new db

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@app/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const adapter = new PrismaPg({
+  connectionString:
+    process.env.PRISMA_DATABASE_POSTGRES_URL ||
+    process.env.DATABASE_URL,
+})
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export { prisma }
